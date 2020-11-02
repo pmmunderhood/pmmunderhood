e9fd8f67ace25ec9e280cde76f077b188f9caca6
Daniil Katson <daniil@beargummy.net>
2020-11-02 21:27:10 +0300

    Add current author

--- authors.js
+++ authors.js
@@ -6,6 +6,7 @@ export default authorId([
   //   first: '',  # id of first tweet
   //   post: false # Include only for author that hasn't finished his week yet. Will display link to twitter instead of archive
   // },
+  { username: 'ulanKZ', start: '2 Nov 2020', first: '1323206520784506881', post: false },
   { username: 'khramushinaa', start: '19 Oct 2020', first: '1318143663025827840' },
   { username: 'NatashaKatson', start: '1 Oct 2020', first: '1311739136601186306' },
 ]);
