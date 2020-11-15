1c4092c1c150fd9985c9a0150f6be8dcb308847f
Natasha <natasha.katson@gmail.com>
2020-11-15 17:37:55 +0300

    Update authors.js

--- authors.js
+++ authors.js
@@ -6,7 +6,7 @@ export default authorId([
   //   first: '',  # id of first tweet
   //   post: false # Include only for author that hasn't finished his week yet. Will display link to twitter instead of archive
   // },
-  { username: 'ryababukha', start: '9 Nov 2020', first: '1325741303087378434', post: false },
+  { username: 'ryababukha', start: '9 Nov 2020', first: '1325741303087378434' },
   { username: 'ulanKZ', start: '2 Nov 2020', first: '1323206520784506881' },
   { username: 'khramushinaa', start: '19 Oct 2020', first: '1318143663025827840' },
   { username: 'NatashaKatson', start: '1 Oct 2020', first: '1311739136601186306' },
