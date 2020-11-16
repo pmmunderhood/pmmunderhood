a3770ef7bb600c14d4d47cc1efd2fbb2b8bb36f1
Natasha <natasha.katson@gmail.com>
2020-11-17 00:08:32 +0300

    Update authors.js (#7)
    * Update authors.js
    
    * Update authors.js (#8)
    
    * Update authors.js
    
    * Update authors
    
    Co-authored-by: pmmunderhood <ci@github>
    
    Co-authored-by: pmmunderhood <ci@github>

--- authors.js
+++ authors.js
@@ -6,6 +6,7 @@ export default authorId([
   //   first: '',  # id of first tweet
   //   post: false # Include only for author that hasn't finished his week yet. Will display link to twitter instead of archive
   // },
+  { username: 'luisarr', start: '16 Nov 2020', first: '1328237241425145856', post: false },
   { username: 'ryababukha', start: '9 Nov 2020', first: '1325741303087378434' },
   { username: 'ulanKZ', start: '2 Nov 2020', first: '1323206520784506881' },
   { username: 'khramushinaa', start: '19 Oct 2020', first: '1318143663025827840' },
